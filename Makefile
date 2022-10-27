810f9c981d1404e328f69e5c4c42b2e999cb7539
Xin Lei <xinleibird@gmail.com>
2022-10-28 04:37:10 +0800

    update

--- Makefile
+++ Makefile
@@ -18,3 +18,6 @@ update-force: ## Forcefully pull all changes and don't ask to patch
 
 serve: ## Serve Quartz locally
 	hugo-obsidian -input=content -output=assets/indices -index -root=. && hugo server --enableGitInfo --minify
+
+dev: ## Serve Quartz locally
+	hugo-obsidian -input=/Users/xinlei/Library/Mobile\ Documents/iCloud~md~obsidian/Documents -output=assets/indices -index -root=. && hugo server --enableGitInfo --minify
--- assets/js/util.js
+++ assets/js/util.js
@@ -36,7 +36,7 @@ const removeMarkdown = (
       .replace(/\s{0,2}\[.*?\]: .*?$/g, "")
       .replace(/\!\[(.*?)\][\[\(].*?[\]\)]/g, options.useImgAltText ? "$1" : "")
       .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, "<a>$1</a>")
-      .replace(/!?\[\[\S[^\[\]\|]*(?:\|([^\[\]]*))?\S\]\]/g, "<a>$1</a>")
+      .replace(/!?\[\[\S[^\[\]\|]*(?:\|([^\[\]]*))?\S*\]\]/g, "<a>$1</a>")
       .replace(/^\s{0,3}>\s?/g, "")
       .replace(/(^|\n)\s{0,3}>\s?/g, "\n\n")
       .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, "")
@@ -102,8 +102,9 @@ const highlight = (content, term) => {
     })
     .join(" ")
     .replaceAll('</span> <span class="search-highlight">', " ")
-  return `${startIndex === 0 ? "" : "..."}${mappedText}${endIndex === splitText.length ? "" : "..."
-    }`
+  return `${startIndex === 0 ? "" : "..."}${mappedText}${
+    endIndex === splitText.length ? "" : "..."
+  }`
 }
 
 // Common utilities for search
@@ -189,17 +190,16 @@ const displayResults = (finalResults, extractHighlight = false) => {
   } else {
     results.innerHTML = finalResults
       .map((result) => {
-          if (extractHighlight) {
-            return resultToHTML({
-              url: result.url,
-              title: highlight(result.title, term),
-              content: highlight(removeMarkdown(result.content), term)
-            })
-          } else {
-            return resultToHTML(result)
-          }
+        if (extractHighlight) {
+          return resultToHTML({
+            url: result.url,
+            title: highlight(result.title, term),
+            content: highlight(removeMarkdown(result.content), term),
+          })
+        } else {
+          return resultToHTML(result)
         }
-      )
+      })
       .join("\n")
     const anchors = [...document.getElementsByClassName("result-card")]
     anchors.forEach((anchor) => {
